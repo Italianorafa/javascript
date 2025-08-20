254c1ff30650043f796f184198330123352846ac
Italianorafa <italianorafap@gmail.com>
2025-08-20 16:36:26 +0100

    starting calculator

---
+++ exercicios/Calculator/index.js
@@ -0,0 +1,11 @@
+const numeros = document.querySelectorAll('.number');
+const operacoes = document.querySelectorAll('.operation');
+const limpar = document.querySelector('#clean');
+const deletar = document.querySelector('#delete');
+const mostrar = document.getElementById('show-numbers');
+const botoes = document.getElementById('buttons');
+
+
+function showNum(){
+    
+}
--- exercicios/GitHub_fetch/index.js
+++ exercicios/GitHub_fetch/index.js
@@ -2,7 +2,31 @@ const username = document.querySelector('input#username');
 const pesquisar = document.querySelector('button#pesquisa');
 const res = document.getElementById('resultado');
 
-
-let nome = username.value;
-
-res.innerHTML = `o nome é ${nome}`
+pesquisar.addEventListener('click', () => {
+    fetch(`https://api.github.com/users/${username.value}`)
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Usuário nao encontrado')
+        }
+        return response.json();
+    })
+    .then(data => {
+        res.innerHTML = ''
+        console.log(data);
+        let username = document.createElement('p');
+        let name = document.createElement('p');
+        let bio = document.createElement('p');
+        let id = document.createElement('p');
+        username.innerText = data.login;
+        name.innerText = data.name;
+        bio.innerText = data.bio;
+        id.innerText = data.id;
+        res.appendChild(username);
+        res.appendChild(name);
+        res.appendChild(bio);
+        res.appendChild(id);
+    })
+    .catch(error => {
+        console.error('Erro', error);
+    })
+})
